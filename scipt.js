272de7dd721812e023397a731be8cb436e78e7b6
adulll09 <metingmeet6@gmail.com>
2025-08-19 15:35:55 +0700

    first commit

---
+++ scipt.js
@@ -0,0 +1,7 @@
+function goToSlide(carouselId, index) {
+    var carousel = new bootstrap.Carousel(document.getElementById(carouselId));
+    carousel.to(index);
+  }function goToSlide(carouselId, index) {
+    var carousel = new bootstrap.Carousel(document.getElementById(carouselId));
+    carousel.to(index);
+  }
